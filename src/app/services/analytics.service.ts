8129ab71668fe6c6a929e301eb4f29fdfa9ef597
Greg Hamilton <greghamilton@gregs-mbp.home>
2019-10-16 19:44:54 +0100

    Added Mixpanel analytics service

---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+
+import * as mixpanel from 'mixpanel-browser'; // https://www.npmjs.com/package/mixpanel-browser
+
+import { Listing } from '../interfaces/listing.interface';
+
+/*
+  Analytics provided by Mixpanel
+  Using the Mixpanel Browser client side JS library
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+
+  constructor() { }
+
+  init() {
+    mixpanel.init('405dc69c3a60c398be64b64be6c791f9');
+    console.log('Initialising Mixpanel analytics');
+  }
+
+  aliasUser(uid: string) {
+    // Alias the user ID so Mixpanel associates the user's auth ID (Firebase auto assigned)
+    // with their original cookie based Mixpanel distinctID (Mixpanel auto assigned).
+    mixpanel.alias(uid);
+  }
+
+  identifyUser(uid: string) {
+    mixpanel.identify(uid);
+  }
+
+  registerUser(method: string, accountType: 'user' | 'business') {
+    mixpanel.track('Registered', {
+      method,
+      accountType
+    });
+  }
+
+  signIn(uid: string, method: string, email: string) {
+    mixpanel.track('Signed In', {
+      uid,
+      method,
+      email
+    });
+  }
+
+  signOut() {
+    mixpanel.track('Signed Out');
+  }
+
+}
